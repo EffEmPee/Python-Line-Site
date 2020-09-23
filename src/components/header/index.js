2dcbe7a5d07d91f420f263b980c5ea523f23ee76
EffEmPee <felipemacacari@gmail.com>
2020-09-22 21:34:06 -0300

    responsive-manu-bar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import './styles.css';
 
-import logo from '../../img/preta.png';
+import logo from '../../img/logo.png';
 import about from '../../img/about.png';
 import group from '../../img/team.png';
 import projects from '../../img/box.png';
@@ -14,34 +14,48 @@ export default class Header extends Component {
   render() {
 
     return (
-      <div className="sideBar">
-        <div className="container">
-          <img id="logo" alt="logo" src={ logo } />
-          <h1 className="name">PYTHON LINE</h1>
-        </div>
-        <ul className="buttons">
-          <li className="button" style={{"marginTop": "0"}}>
-            <img href="" alt="Aboutus" src={ about } />  
-              <div className="text"> <p>Quem somos</p></div>
+      <nav className="sideBar">
+        <ul className="sideBar-nav">      
+          <li className="logo">
+            <a href="" className="logo-link">
+              <img alt="logo" src={ logo } />
+              <span className="link-text logo-text">Python line</span>
+
+            </a>
+            {/* <i className="fas fa-angle-double-right"></i> */}
           </li>
           <li className="button">
-            <img href="" alt="group" src={ group } />  
-            <p className="text" >Participantes</p>
+            <a href="" className="button-link">
+              <img href="" alt="Aboutus" src={ about } />  
+              <span className="link-text">Quem somos</span>
+            </a>
           </li>
           <li className="button">
-            <img href="" alt="projects" src={ projects } />  
-            <p className="text">Projetos</p>
+            <a href="" className="button-link">
+              <img href="" alt="group" src={ group } />  
+              <span className="link-text" >Participantes</span>
+            </a>
           </li>
           <li className="button">
-            <img href="" alt="help" src={ help } />  
-            <p className="text">Como Ajudar</p>
+            <a href="" className="button-link">
+              <img href="" alt="projects" src={ projects } />  
+              <span className="link-text">Projetos</span>
+            </a> 
           </li>
           <li className="button">
-            <img href="" alt="contact" src={ contact } />  
-            <p className="text">Contato</p>
+            <a href="" className="button-link">
+              <img href="" alt="help" src={ help } />  
+              <span className="link-text">Como Ajudar</span>
+            </a>
+          </li>
+          <li className="button">
+            <a href="" className="button-link">
+              <img href="" alt="contact" src={ contact } />  
+              <span className="link-text">Contato</span>
+            </a>
           </li>
         </ul>
-      </div>
+      </nav>
     )
   }
 };
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -12,6 +12,17 @@ export default class Main extends Component {
         <h1>
           Pagina aqui 
         </h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae varius dui. Quisque quis sagittis urna. Morbi ac ullamcorper dui. Quisque in erat purus. Cras quis fringilla leo. In nec luctus ligula, quis dignissim felis. Nulla maximus lobortis congue. Sed sollicitudin, eros a venenatis tempus, augue nulla placerat tellus, semper gravida orci ante in libero. Vestibulum maximus semper turpis, ut rutrum quam consequat id. Quisque condimentum velit leo, sit amet tempor erat feugiat venenatis.
+
+Vestibulum eu placerat ex. Aliquam eleifend, urna ut efficitur varius, mauris ante egestas orci, et congue nisi enim at turpis. Mauris volutpat, neque vel sollicitudin porttitor, ante lectus venenatis ex, sit amet ornare neque libero vitae quam. Aenean porta ut sapien eu consequat. Vestibulum interdum sagittis urna sed hendrerit. Cras vel leo nulla. Vestibulum eu neque in tortor convallis fermentum vitae a est. Donec porttitor tristique bibendum. Ut vulputate congue massa eu iaculis. Mauris purus nibh, blandit vel diam et, egestas semper quam. Morbi malesuada nisi tincidunt ligula finibus elementum. Praesent non nunc lacinia, aliquam dui a, imperdiet dolor. Etiam ornare sodales nibh, non cursus lacus pretium et. Proin quis elit pulvinar nulla ornare dapibus ac eu enim. Vestibulum sed leo in orci consequat tempor.
+
+Proin sagittis efficitur nunc congue bibendum. Cras neque erat, mattis ac tortor vitae, scelerisque pharetra nisi. Fusce at ornare velit. Nullam quis sodales mi. Pellentesque tempor sollicitudin arcu, eget finibus lectus viverra eget. Integer rhoncus maximus mattis. Praesent imperdiet erat euismod, pretium leo at, porta tortor. Aenean cursus felis quis ante dictum, ac ultrices nunc tempus. In nisi eros, porttitor ut tortor sed, malesuada auctor nisi. Nunc auctor sollicitudin pulvinar. Praesent interdum ex vitae erat interdum iaculis. Morbi maximus nisi quis eros congue, vel efficitur libero finibus.
+
+Pellentesque gravida risus at enim pulvinar pretium. In id orci vel arcu dapibus blandit. Integer ac lectus massa. Nullam viverra molestie sem, ac rutrum nibh tempus ac. Cras at nisi sed eros feugiat consequat. Duis sed facilisis dolor. Duis elit justo, porta sit amet ex vel, tristique mollis augue. In ut scelerisque sem. Mauris id neque sed est bibendum volutpat eget quis nulla. Proin nec enim dui. Nullam eu elit ut tellus semper commodo sed a diam. In sed venenatis dolor.
+
+Fusce ac nisl vitae mi vulputate euismod quis quis nibh. Mauris a risus tristique, feugiat eros quis, auctor massa. Cras varius augue a felis sollicitudin, a sodales tellus egestas. Aenean tempus sapien sed ornare scelerisque. Donec at massa in velit scelerisque rhoncus ut ac enim. Morbi nec libero libero. Ut convallis leo in nisi tincidunt aliquam. Nulla facilisi. Praesent ut accumsan arcu, eget egestas massa. Integer vitae fermentum ante, sit amet vestibulum risus. Vestibulum dictum facilisis libero, a volutpat ex. Cras iaculis lacus eu quam pellentesque, eu ultricies massa dictum. Fusce non orci tristique, faucibus magna quis, laoreet purus. Nulla ullamcorper orci sed ex pharetra, ut viverra mauris blandit. Donec scelerisque aliquet consequat.
+        </p>
       </main>
       
     );
