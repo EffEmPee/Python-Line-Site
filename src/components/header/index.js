42f71378f6761efa240e5bae16185efe36d459cb
EffEmPee <felipemacacari@gmail.com>
2020-09-22 22:07:21 -0300

    responsive-manu-bar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -17,7 +17,7 @@ export default class Header extends Component {
       <nav className="sideBar">
         <ul className="sideBar-nav">      
           <li className="logo">
-            <a href="" className="logo-link">
+            <a href="#" className="logo-link">
               <img alt="logo" src={ logo } />
               <span className="link-text logo-text">Python line</span>
 
@@ -25,32 +25,26 @@ export default class Header extends Component {
             {/* <i className="fas fa-angle-double-right"></i> */}
           </li>
           <li className="button">
-            <a href="" className="button-link">
-              <img href="" alt="Aboutus" src={ about } />  
+            <a href="#" className="button-link">
+              <img alt="Aboutus" src={ about } />  
               <span className="link-text">Quem somos</span>
             </a>
           </li>
           <li className="button">
-            <a href="" className="button-link">
-              <img href="" alt="group" src={ group } />  
-              <span className="link-text" >Participantes</span>
-            </a>
-          </li>
-          <li className="button">
-            <a href="" className="button-link">
-              <img href="" alt="projects" src={ projects } />  
+            <a href="#" className="button-link">
+              <img alt="projects" src={ projects } />  
               <span className="link-text">Projetos</span>
             </a> 
           </li>
           <li className="button">
-            <a href="" className="button-link">
-              <img href="" alt="help" src={ help } />  
+            <a href="#" className="button-link">
+              <img alt="help" src={ help } />  
               <span className="link-text">Como Ajudar</span>
             </a>
           </li>
           <li className="button">
-            <a href="" className="button-link">
-              <img href="" alt="contact" src={ contact } />  
+            <a href="#" className="button-link">
+              <img alt="contact" src={ contact } />  
               <span className="link-text">Contato</span>
             </a>
           </li>
